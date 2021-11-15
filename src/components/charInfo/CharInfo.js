59806ef19f6c66f52148c25f6fc46e28d9907e07
AntonBoreiko <1213.96@mail.ru>
2021-11-15 23:21:55 +0300

    Fix project/ add in project finite-state machine

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react'
 import useMarvelService from '../../services/MarvelService'
 import PropTypes from 'prop-types'
-import Spinner from '../spinner/Spinner'
-import ErrorMessage from '../errorMessage/ErrorMessage'
-import Skeleton from '../skeleton/Skeleton'
+import setContent from '../../utils/setContent'
 
 
 
@@ -11,7 +9,7 @@ import './charInfo.scss'
 
 const CharInfo = (props) => {
     const [char, setChar] = useState(null)
-    const { loading, error, getCharacter, clearError } = useMarvelService()
+    const { getCharacter, clearError, process, setProcess } = useMarvelService()
 
     useEffect(() => {
         updateChar()
@@ -26,32 +24,22 @@ const CharInfo = (props) => {
         clearError()
         getCharacter(charId)
             .then(onCharLoaded)
+            .then(() => setProcess('confirmed'))
     }
 
     const onCharLoaded = (char) => {
         setChar(char)
     }
 
-
-    const skeleton = char || loading || error ? null : <Skeleton />
-    const errorMessage = error ? <ErrorMessage /> : null
-    const spinner = loading ? <Spinner /> : null
-    const content = !(loading || error || !char) ? <View char={char} /> : null
-
     return (
         <div className="char__info">
-            {skeleton}
-            {errorMessage}
-            {spinner}
-            {content}
-
-
+            {setContent(process, View, char)}
         </div>
     )
 }
 
-const View = ({ char }) => {
-    const { name, description, thumbnail, home, wiki, comics } = char
+const View = ({ data }) => {
+    const { name, description, thumbnail, home, wiki, comics } = data
     let noComics = 'Comics:'
     if (comics == 0) {
         noComics = 'Comics: There is no comics'
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+
 import Spinner from '../spinner/Spinner'
 import ErrorMessage from '../errorMessage/ErrorMessage'
 import useMarvelService from '../../services/MarvelService'
@@ -6,9 +7,26 @@ import PropTypes from 'prop-types'
 
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-
-
 import './charList.scss'
+
+const setContent = (process, Component, newItemLoading) => {
+    switch (process) {
+        case 'waiting':
+            return <Spinner />
+            break;
+        case 'loading':
+            return newItemLoading ? <Component /> : <Spinner />
+            break;
+        case 'confirmed':
+            return <Component />
+            break;
+        case 'error':
+            return <ErrorMessage />
+            break;
+        default:
+            throw new Error('Unexpected process state')
+    }
+}
 
 const CharList = (props) => {
 
@@ -17,7 +35,7 @@ const CharList = (props) => {
     const [offset, setOffset] = useState(210)
     const [charEnded, setCharEnded] = useState(false)
 
-    const { loading, error, getAllCharacters } = useMarvelService()
+    const { loading, error, getAllCharacters, process, setProcess } = useMarvelService()
 
     useEffect(() => {
         onRequest(offset, true)
@@ -28,6 +46,7 @@ const CharList = (props) => {
 
         getAllCharacters(offset)
             .then(onCharListLoaded)
+            .then(() => setProcess('confirmed'))
     }
 
 
@@ -93,16 +112,9 @@ const CharList = (props) => {
         )
     }
 
-    const items = renderItems(charList)
-
-    const errorMessage = error ? <ErrorMessage /> : null
-    const spinner = loading && !newItemLoading ? <Spinner /> : null
-
     return (
         <div className="char__list">
-            {errorMessage}
-            {spinner}
-            {items}
+            {setContent(process, () => renderItems(charList), newItemLoading)}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
